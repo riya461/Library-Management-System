99c9e940b3408344e6ffb72c7c4d2ae1a7c2f271
Srutav Tarun <srutavtarun@gmail.com>
2023-10-10 14:45:57 +0530

    added table

---
+++ lms/src/pages/landing.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export const Landing = () => {
+    return (
+        <div>
+            <table border="1">
+        <thead>
+            <tr>
+                <th>Book ID</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Available</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>The Great Gatsby</td>
+                <td>F. Scott Fitzgerald</td>
+                <td>Yes</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>To Kill a Mockingbird</td>
+                <td>Harper Lee</td>
+                <td>No</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>1984</td>
+                <td>George Orwell</td>
+                <td>Yes</td>
+            </tr>
+        </tbody>
+    </table>
+        </div>
+    );
+};
