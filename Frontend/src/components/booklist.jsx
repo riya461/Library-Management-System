834133041851fdfdf1a7bf308260e5dd4bca809f
Abhishek S <abhisheks3002@gmail.com>
2023-12-06 08:00:07 +0530

    Refactor booklist component

--- Frontend/src/components/booklist.jsx
+++ Frontend/src/components/booklist.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { BsPlus, BsTrash, BsSearch } from "react-icons/bs";
 import { IoCloseSharp } from "react-icons/io5";
 import "./styles/booklist.css";
-<<<<<<< HEAD
 
 const BookList = () => {
   // search 
@@ -18,26 +17,6 @@ const BookList = () => {
     total: 0,
   });
   const addBookVal = () => {
-=======
-import DeleteBook from "./deletebook";
-// import AddCopy from "./addcopy";
-
-const BookList = () => {
-  
-  const [searchTerm, setSearchTerm] = useState("");
-  const [books, setBookList] = useState([]);
-  const [showAddForm, setShowAddForm] = useState(false);
-  const [showDeleteForm, setShowDeleteForm] = useState(false);
-
-  const [newBook, setNewBook] = useState({
-    isbn: "",
-    title: "",
-    author: "",
-    available: 0,
-    total: 0,
-  });
-const addBookVal = () => {
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
   fetch("http://localhost:5000/api/books/get")
     .then(res => res.json())
     .then(
@@ -56,11 +35,6 @@ const addBookVal = () => {
         console.log(result);
       })
       },[]);
-<<<<<<< HEAD
-=======
-
-  
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
   const toggleAddForm = () => {
     setShowAddForm(!showAddForm);
   };
@@ -78,36 +52,12 @@ const addBookVal = () => {
       console.log(newBook);
       setShowAddForm(false);
       addBookVal();
-<<<<<<< HEAD
-=======
 
       console.log(newBook)
     } catch (err) {
       console.error(err.message);
     }
   };
-  
-  const handleAddBook = () => {
-    console.log("On handle",newBook);
-
-    // Validate that the ID is provided
-    if (!newBook.isbn) {
-      alert("Please provide a Book ID.");
-      return;
-    }
-    
-    onSubmitAdd();
-
-
-  }
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
-
-      console.log(newBook)
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-<<<<<<< HEAD
   const handleAddBook = () => {
     console.log("On handle",newBook);
     onSubmitAdd();
@@ -208,8 +158,6 @@ const addBookVal = () => {
     
    }
   
-=======
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
   return (
 
     <main className="booklist-main-container">
@@ -228,11 +176,7 @@ const addBookVal = () => {
         <h3>BOOKS</h3>
         <div className="booklist-buttons-container">
           <button className="booklist-delete-button" 
-<<<<<<< HEAD
           onClick={toggleDeleteForm}
-=======
-          // onClick={toggleDeleteForm}
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
           >
             <BsTrash /> Delete a Book
           </button>
@@ -244,11 +188,7 @@ const addBookVal = () => {
           <button
             className="booklist-add-copy-button"
 
-<<<<<<< HEAD
             onClick={toggleAddCopyForm}
-=======
-            // onClick={toggleAddCopyForm}
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
           >
             <BsPlus /> Add New Copy
           </button>
@@ -273,13 +213,8 @@ const addBookVal = () => {
             )
             .map((book)  => (
               console.log(book),
-<<<<<<< HEAD
               <tr key={book.book_id}>
                 <td>{book.book_id}</td>
-=======
-              <tr key={book.isbn}>
-                <td>{book.isbn}</td>
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
                 <td>{book.title}</td>
                 <td>{book.author}</td>
                 <td>{book.available}</td>
@@ -309,17 +244,7 @@ const addBookVal = () => {
             </button>
           </div>
 
-<<<<<<< HEAD
           
-=======
-          <label>Book ID:</label>
-          <input
-            type="text"
-            value={newBook.isbn}
-            onChange={(e) => setNewBook({ ...newBook, isbn: e.target.value })}
-          />
-
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
           <label>Title:</label>
           <input
             type="text"
@@ -338,37 +263,18 @@ const addBookVal = () => {
           <label>Total Copies:</label>
           <input
             type="number"
-<<<<<<< HEAD
             value={newBook.total}
             onChange={(e) =>
               setNewBook({ ...newBook, total: e.target.value })
             }
           />
 
-=======
-            value={newBook.available}
-            onChange={(e) =>
-              setNewBook({ ...newBook, available: e.target.value })
-            }
-          />
-
-          <label>Total Copies:</label>
-          <input
-            type="number"
-            value={newBook.total}
-            onChange={(e) =>
-              setNewBook({ ...newBook, total: e.target.value })
-            }
-          />
-
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
           <button className="center-align-button" 
           onClick={handleAddBook}
           >
             Add Book
           </button>
         </div>
-<<<<<<< HEAD
         )
       }
 
@@ -458,27 +364,6 @@ const addBookVal = () => {
 
 
       }
-=======
-      )
-      }
-
-      {/* Overlay for DeleteBook */}
-      {showDeleteForm && (
-        <DeleteBook
-          onClose={() => setShowDeleteForm(false)} // Make sure onClose is passed correctly
-          onDelete={handleDeleteConfirm}
-          
-        />
-      )}
-
-      {/* {showAddCopyForm && (
-        <AddCopy
-          onClose={() => setShowAddCopyForm(false)}
-          onAddCopy={handleAddCopy}
-          selectedBooks={selectedBooksForCopy}
-        />
-      )} */}
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
 
     </main>
   );
@@ -486,97 +371,4 @@ const addBookVal = () => {
 
 
 export default BookList;
-<<<<<<< HEAD
  
-=======
-
-
-  // const [showAddCopyForm, setShowAddCopyForm] = useState(false);
-  // const [selectedBooksForCopy, setSelectedBooksForCopy] = useState([]);
-  //  const toggleAddCopyForm = () => {
-  //   setShowAddCopyForm(!showAddCopyForm);
-  // };
-  
-  // const handleAddCopy = (copiesToAdd) => {
-  //   // Implement your logic to add copies to the selected books
-  //   console.log(`Adding ${copiesToAdd} copies to selected books`);
-  //   // Update the book list accordingly
-  //   // ...
-
-  //   // Reset the selected books for copy
-  //   // setSelectedBooksForCopy([]);
-
-  //   // Close the add copy form
-  //   // setShowAddCopyForm(false);
-  // };
-  // useEffect(() => {
-  //   // Update the availability status for each book
-  //   const updatedBookList = initialBooks.map((book) => {
-  //     const availability = getAvailability(book.available);
-  //     return { ...book, availability };
-  //   });
-
-  //   // Sort the book list alphabetically by title
-  //   updatedBookList.sort((a, b) => a.title.localeCompare(b.title));
-
-  //   setBookList(updatedBookList);
-  // }, []);
-
-  // useEffect(() => {
-  //   // Filter and sort the book list based on the search term
-  //   const filteredBooks = initialBooks
-  //     .filter((book) =>
-  //       book.title.toLowerCase().includes(searchTerm.toLowerCase())
-  //     )
-  //     .map((book) => ({
-  //       ...book,
-  //       availability: getAvailability(book.available),
-  //     }));
-
-    // Sort the filtered books alphabetically by title
-  //   filteredBooks.sort((a, b) => a.title.localeCompare(b.title));
-
-  //   setBookList(filteredBooks);
-  // }, [searchTerm]);
-
-  // const getAvailability = (available) => {
-  //   return available > 0 ? "Yes" : "No";
-  // };
-
-
-
-
-  
-
-  //   // Set availability to 'Yes' for the new book
-  //   const newBookWithAvailability = { ...newBook, availability: "Yes" };
-
-  //   // Add the new book to the book list
-  //   const updatedBookList = [...books, newBookWithAvailability].sort(
-  //     (a, b) => a.title.localeCompare(b.title)
-  //   );
-
-  //   setBookList(updatedBookList);
-
-  //   // Close the add form
-  //   setShowAddForm(false);
-
-  //   // Close the delete form
-  //   setShowDeleteForm(false);
-
-
-
-  // const toggleDeleteForm = () => {
-  //   setShowDeleteForm(!showDeleteForm);
-  // };
-
-  
-
-  // // const handleDeleteCancel = () => {
-  // //   // Close the overlay
-  // //   setShowDeleteForm(false);
-  // // };
-
- 
- 
->>>>>>> f0afcbe66a7ca2790ea2e180490f0153a3649ef9
